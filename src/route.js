1b3e2410f581bbe08cf8de6b5b41dbdf5e69ff5e
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-06-10 16:34:52 +0200

    [FEAT] ADDED ROUTER.JS

--- src/route.js
+++ src/route.js
@@ -1,1 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppHome from "./pages/AppHome.vue";
+import AppAbout from "./pages/AppAbout.vue";
+import AppContact from "./pages/AppContact.vue";
+
+const routes = [
+  { path: "/", component: AppHome },
+  { path: "/about", component: AppAbout },
+  { path: "/contact", component: AppContact },
+];
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
