26d9b2d9380813dd31fa1345adf1cc78d727587d
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-06-10 17:16:47 +0200

    chore: Add router links to About and Contact pages in AppAbout.vue

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -4,15 +4,15 @@ export default{
     name: 'AppAbout',
 }
 
-
-
-
 </script>
 <template>
     <!-- IMPORTO ROUTER SU SINGOLO A-HREF -->
   <div>
     <!-- AGGIUNGO ROUTER -->
     <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    
 
     
   </div>
