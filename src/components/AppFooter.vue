f1d95000096d35fbd22d72efa9ac9201ce36fb57
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-06-11 12:32:58 +0200

    [UPD] footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,6 +7,74 @@ export default{
 </script>
 
 <template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-3">
+        <div>
+          Logo
+        </div>
+        <div class="mt-2">
+          1234 Avada Avenue Avadaville
+          info@your-company.com
+          Call us: (1) 234 5678
+        </div>
+      </div>
+      <div class="col-3">
+        <h3>
+          Shop by brand
+        </h3>
+        <ul>
+          <li>a</li>
+          <li>v</li>
+          <li>v</li>
+          <li>d</li>
+        </ul>
+      </div>
+      <div class="col-3  ">
+        <h3 >
+          Shop by brand
+        </h3>
+        <ul>
+          <li>a</li>
+          <li>v</li>
+          <li>v</li>
+          <li>d</li>
+        </ul>
+      </div>
+      <div class="col-3 border rounded">
+        <div class="container">
+          <h3>
+            Subscribe to our newsletter
+          </h3>
+          <div class="mb-2">
+            <input type="text" name="" id="" placeholder="Insert your email..." class="rounded-pill p-2" >
+          </div>
+
+          <div>
+            <button type="button" class="btn btn-success rounded-pill p-2 px-5 mb-2">Success</button>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+    <hr>
+  </div>
+  <div class="col-12 ">
+    <ul class="d-flex flex-row justify-content-center ">
+      <li>a</li>
+      <li>v</li>
+      <li>c</li>
+      <li>b</li>
+      <li>d</li>
+    </ul>
+
+  </div>
+  <div class="col-12">
+    <font-awesome-icon icon="fa-brands fa-facebook-f" />
+  </div>
+
+  
   <h2>
     Footer
   </h2>
