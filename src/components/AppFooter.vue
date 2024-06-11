a144926ea6dfa3b63b54e97b6cbf0197ae26ced9
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-06-11 10:01:37 +0200

    [UPD] AppHeader and AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+
+<script>
+export default{
+    name: 'AppFooter',
+}
+
+</script>
+
+<template>
+  <h2>
+    Footer
+  </h2>
+    <!-- IMPORTO ROUTER SU SINGOLO A-HREF -->
+
+  </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+
+<script>
+export default{
+    name: 'AppHeader',
+}
+
+</script>
+
+<template>
+  <h2>
+    Header
+  </h2>
+    <!-- IMPORTO ROUTER SU SINGOLO A-HREF -->
+
+  </template>
