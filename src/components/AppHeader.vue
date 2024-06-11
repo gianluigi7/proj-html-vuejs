440e9ed7ae6c2b8ad18b74f5f8704f9c6a43ec5d
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-06-11 13:19:35 +0200

    [FEAT] ICONS,STARTING ROUTER LINK

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,7 +16,7 @@ export default{
       <div class="input-group my-2 my-lg-0 d-flex col- position-relative p-5 w-25">
         <input type="text" class="form-control rounded-pill border-0" placeholder="Search..." aria-label="Search" style="padding-left: 50px; padding-top: 5px; padding-bottom: 5px; background-color: #f3f4f3;">
       <button class="btn position-absolute border-0 bg-transparent" style="left: 15px; top: 50%; transform: translateY(-50%);">
-        <font-awesome-icon icon="search" />
+        <font-awesome-icon :icon="['fas', 'search']" />
       </button>
     </div>
     <!-- QUESTION CALL-US -->
@@ -29,8 +29,9 @@ export default{
         </div>
         <!-- aggiungo icone alla fine della navbar -->
         <div class="d-flex justify-content-end align-items-center gap-3 ">
-          <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-          <font-awesome-icon icon="user"></font-awesome-icon>
+          <font-awesome-icon :icon="['fas', 'shopping-cart']"></font-awesome-icon>
+          <font-awesome-icon :icon="['fas', 'user']"></font-awesome-icon>
+
         </div>
   </nav>
     </div>
